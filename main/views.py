a19379eff48b31b863e886191e3106391b45f61f
amenokoe <amiami947gmail.com>
2020-08-12 10:08:38 +0600

    Add post list view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Post
+
+# @api_view()
+# def hello_world(request):
+#     return Response('Hello, world!')
+from .serializers import PostSerializer
+
+
+@api_view()
+def posts_list(request):
+    queryset = Post.objects.all()
+    serializer = PostSerializer(queryset, many=True, context={'request', })
+    serializer.is_valid(raise_exception=True)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
